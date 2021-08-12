5723b0ced29863323172c159355d027b475da32f
jeevan-kishore <kishor080@gmail.com>
2021-08-12 14:08:00 +0530

    add comments

--- pages/_/index.vue
+++ pages/_/index.vue
@@ -6,6 +6,7 @@
 export default {
   async asyncData({ params }) {
     const slug = params.pathMatch;
+    /* TODO: Make calls sequentially to API and return when it's not a 404 / 500, render related template */
     return { slug }
   }
 }
